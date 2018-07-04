3c4e1fc6de2ccffafae0cf09dafce0f22a0285f7
Philip James de Vries <phil@reshiftmedia.com>
2018-07-04 10:39:38 -0400

    added carousel html, js, scss

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,3 +1,50 @@
 (function($){
+    /**
+     * Carousel jQuery
+     */
+    var temparcCarousel = $('.temparc-carousel');
+    var temparcSlide = '.slide';
 
+    temparcCarousel.each( function(){
+
+        $(this).find('.controls span').click( function(){
+
+            // DOM vars
+            var isNext = $(this).hasClass('next');
+            var slides = $(this).closest('.temparc-carousel').find(temparcSlide);
+            var slideNo;
+            var nextSlide;
+
+            // hide current slide
+            $('.slide').each( function( index ) {
+                if( $(this).hasClass('visible') ) {
+                    slideNo = parseInt( $(this).data('slide-count') );
+                    $(this).removeClass('visible');
+                }
+            });
+
+            // next slide
+            if(isNext){
+                // last slide && next
+                if(slideNo == slides.length){
+                    nextSlide = $(temparcSlide + '[data-slide-count="1"]');
+                } else {
+                    nextSlide = $(temparcSlide + '[data-slide-count="' + (slideNo + 1) +'"]');
+                }
+            }
+            // previous slide
+            else{
+                // first slide && previous
+                if(slideNo == 1){
+                    nextSlide = $(temparcSlide + '[data-slide-count="' + slides.length + '"]');
+                } else {
+                    nextSlide = $(temparcSlide + '[data-slide-count="' + (slideNo - 1) +'"]');
+                }
+            }
+
+            nextSlide.addClass('visible');
+
+            return;
+        });
+    });
 })(jQuery);
